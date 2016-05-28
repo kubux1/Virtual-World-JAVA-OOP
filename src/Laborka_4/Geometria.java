a8b232b7d870ac1aa4599ef19c30cfeb45116ba7
kubux1 <jakubkotow@gmail.com>
2016-05-28 11:45:52 +0200

    Add all my files

--- src/Laborka_4/Geometria.java
+++
@@ -1,9 +0,0 @@
-package Laborka_4;
-
-/**
- * Created by Kuba on 2016-04-02.
- */
-interface Geometria {
-    double pole();
-    double obwod();
-}
--- src/SymulatorMaszynyLosujacej/Kula.java
+++
@@ -1,9 +0,0 @@
-package SymulatorMaszynyLosujacej;
-
-/**
- * Created by Kuba on 2016-03-17.
- */
-public class Kula {
-     public int LiczbaNaKuli;
-   public  boolean czy_dociazona;
-}
